5f1ffc432f9ae8aa01dc5a3289569932ff461300
saito-kosuke-acesystems <saito-kosuke@acesystems.co.jp>
2025-05-30 14:15:35 +0900

    アップロードしたファイルをベクトルDBに保存する処理を追加

--- pglite_vector_search/src/components/ChatForm.vue
+++ pglite_vector_search/src/components/ChatForm.vue
@@ -5,15 +5,28 @@ import { useChatStore } from '@/stores/chatMessage'
 const chatStore = useChatStore()
 
 const sendMessage = ref('')
+const uploadedFile = ref<File | null>(null)
+
 const isLoading = computed((): boolean => {
     return chatStore.isLoading
 })
 
 const onSendMessage = (): void => {
     chatStore.isLoading = true
-    chatStore.addUserMessage(sendMessage.value)
+    chatStore.addMessage(sendMessage.value)
     chatStore.getBotReply(sendMessage.value)
     sendMessage.value = ''
+}
+
+const onFileChange = async (event: Event) => {
+    const target = event.target as HTMLInputElement
+    if (target.files && target.files.length > 0) {
+        uploadedFile.value = target.files[0]
+        chatStore.isLoading = true
+        chatStore.addMessage(`ファイルをアップロードしています: ${uploadedFile.value.name}`, true)
+        await chatStore.uploadFile(uploadedFile.value)
+        uploadedFile.value = null
+    }
 }
 
 onUpdated((): void => {
@@ -26,8 +39,12 @@ onUpdated((): void => {
 
 <template>
     <form v-on:submit.prevent="onSendMessage" class="send-form">
+        <label class="file-upload-label">
+            <input type="file" @change="onFileChange" class="file-input" />
+            <span class="submit-btn">RAG追加</span>
+        </label>
         <input type="text" id="sendMessage" v-model="sendMessage" required class="input-text" />
-        <button type="submit" v-bind:disabled="isLoading">送信</button>
+        <button type="submit" v-bind:disabled="isLoading" class="submit-btn" >送信</button>
     </form>
 </template>
 
@@ -54,5 +71,31 @@ onUpdated((): void => {
     background-color: white;
     box-shadow: 0 -1px 5px rgba(0, 0, 0, 0.3);
     z-index: 1;
+    align-items: center;
+}
+
+.file-upload-label {
+    display: flex;
+    align-items: center;
+    margin-right: 10px;
+    cursor: pointer;
+    position: relative;
+}
+
+.file-input {
+    display: none;
+}
+
+.submit-btn {
+    font-size: 20px;
+    padding: 4px 8px;
+    border-radius: 4px;
+    background: #f0f0f0;
+    border: 1px solid #ccc;
+    transition: background 0.2s;
+}
+
+.file-upload-btn:hover {
+    background: #e0e0e0;
 }
 </style>
--- pglite_vector_search/src/stores/chatMessage.ts
+++ pglite_vector_search/src/stores/chatMessage.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import { MessageData, SendMessage, State } from '@/models/chatMessage'
 import { generateChatMessage, generateEmbedding } from '@/utils/openAI'
+import { insertMemory } from '@/utils/pglite'
 
 export const useChatStore = defineStore(
     'chat',
@@ -12,12 +13,12 @@ export const useChatStore = defineStore(
         getters: {},
         actions: {
             // ユーザの入力したメッセージをmessageListに追加
-            addUserMessage(question: string) {
+            addMessage(question: string, isBot: boolean = false) {
                 const setId = this.messageList.size + 1
                 this.messageList.set(setId, {
                     id: setId,
                     message: question,
-                    isBot: false
+                    isBot: isBot
                 })
             },
             // ボットから回答を受けメッセージを追加
@@ -48,6 +49,47 @@ export const useChatStore = defineStore(
                     .finally(() => {
                         this.isLoading = false
                     })
+            },
+            async uploadFile(file: File) {
+                // 拡張子を確認
+                const ext = file.name.split('.').pop()?.toLowerCase()
+                if (!ext || (ext !== 'txt')) {
+                    this.addMessage('TXTファイルをアップロードしてください。', true)
+                    this.isLoading = false
+                    return
+                }
+                // テキストファイルをベクトル化
+                const text = await file.text()
+                const vectorText = await generateEmbedding(text)
+                    .catch((reason) => {
+                        if (reason instanceof Error) {
+                            console.error(reason.message, reason.stack)
+                            window.alert(reason.message)
+                        } else {
+                            console.error('Error occured.', reason)
+                            window.alert('エラー発生')
+                        }
+                    })
+
+                // ベクトル化結果をpgliteに保存
+                if (vectorText) {
+                    await insertMemory(text, vectorText)
+                        .then(() => {
+                            this.addMessage('ファイルをアップロードしました。', true)
+                        })
+                        .catch((reason) => {
+                            if (reason instanceof Error) {
+                                console.error(reason.message, reason.stack)
+                                window.alert(reason.message)
+                            } else {
+                                console.error('Error occured.', reason)
+                                window.alert('エラー発生')
+                            }
+                        })
+                } else {
+                    this.addMessage('ファイルのアップロードに失敗しました。', true)
+                }
+                this.isLoading = false
             }
         }
     })
